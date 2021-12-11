cf6fb950d8190b3eac898d218b3c394870c24c59
GithubUserNotABot <69889510+GithubUserNotABot@users.noreply.github.com>
2021-12-11 03:29:10 -0500

    Add files via upload

---
+++ TODO.py
@@ -0,0 +1,9 @@
+# TODO: In the future, color is something I would like to be able to mess with
+# -- (a) if you click on an object you will highlight it and you can change the color by doing that
+# TODO: Be able to mouse over a block and left click it to drag it
+# TODO: Make end levels actually do something
+
+# TODO: be able to make free form objects
+# -- (a) maybe by making the "inside" of the object full with the least amount of rects as possible with no, or at least as little as possible overlap
+
+# todo: maybe ~~~ make checkpoints tbh I don't know how they will feel in the game
