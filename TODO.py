6bcdb15bfa321d470fc41344a754d5f1c4b6e9b7
GithubUserNotABot <69889510+GithubUserNotABot@users.noreply.github.com>
2021-12-15 10:44:08 -0500

    Add files via upload

--- TODO.py
+++ TODO.py
@@ -2,8 +2,12 @@
 # -- (a) if you click on an object you will highlight it and you can change the color by doing that
 # TODO: Be able to mouse over a block and left click it to drag it
 # TODO: Make end levels actually do something
+# -- (a) if you collide with an end level it detects if it was loaded or not (the level your in)
 
 # TODO: be able to make free form objects
-# -- (a) maybe by making the "inside" of the object full with the least amount of rects as possible with no, or at least as little as possible overlap
+# -- (a) maybe by making the "inside" of the object full with the least amount of rects as possible with no,
+# or at least as little as possible overlap
 
 # todo: maybe ~~~ make checkpoints tbh I don't know how they will feel in the game
+
+# todo: get text input from the screen
--- level_editor.py
+++ level_editor.py
@@ -1,14 +1,15 @@
 import pygame.mouse
 import add_object__
+import os
 
 rect_list = None
-step = 2
-counter_file = 0
 BeginLevel = None
 EndLevel = None
 
 def save_level():
-    global rect_list, BeginLevel, EndLevel
+    global rect_list, BeginLevel, EndLevel, file_name
+    file_name = None
+
     rect_list = add_object__.get_objects()
     beginEnd = add_object__.get_beginEnd_()
     # flags
@@ -25,15 +26,28 @@ def save_level():
             print("\nYou need to have a End level\nPress 2 to make one ")
             return Exception
 
-    BeginLevel = [beginEnd[0][1], beginEnd[0][2]]
-    EndLevel = [beginEnd[1][1], beginEnd[1][2]]
-    rect_list.remove((beginEnd[0]))
-    rect_list.remove((beginEnd[1]))
-
+    # get user to save as or to a project
+    user = input("say: \"[project name]\" : ")
+    files = os.listdir(path='levels')
+    findList = []
+    for i in range(len(os.listdir(path='levels'))):
+        if files[i].find(".txt") != -1:
+            findList.append(files[i])
+    # finds all txt files and puts it in findList
+    for i in range(len(findList)):
+        if (user + ".txt") == findList[i]:
+            file_name = user + ".txt"
+    if file_name is None:
+        print("couldn't find file, please use \"6\" to create file")
+        return
 
     if rect_list:
         user = input("Are you sure you want to save the level? (Y or N) : ")
         if user.upper() == "Y":
+            BeginLevel = [beginEnd[0][1], beginEnd[0][2]]
+            EndLevel = [beginEnd[1][1], beginEnd[1][2]]
+            rect_list.remove((beginEnd[0]))
+            rect_list.remove((beginEnd[1]))
 
             game_object = {
                 "end level": EndLevel,
@@ -41,41 +55,72 @@ def save_level():
                 "obstacles": rect_list,
             }
 
-            file = open("levels.txt", 'a')
-            file.write(str(game_object) + "\n")
+            file_name = "levels/" + file_name
+            file = open(file_name, 'a')
+            file.write((str(game_object) + "\n"))
             file.close()
             print(" == Saved Successfully! == ")
             add_object__.make(BeginLevel[0], (BeginLevel[1][0], BeginLevel[1][1]), (BeginLevel[1][2], BeginLevel[1][3]), cntrl="begin level")
             add_object__.make(EndLevel[0], (EndLevel[1][0], EndLevel[1][1]), (EndLevel[1][2], EndLevel[1][3]), cntrl="end level")
-            return None
-
+            return
+    if user.upper() != "Y":
+        print("okay, returning to game...")
+        return
 def load_level():
     # Seeing how many lines there are in the file
-    global counter_file, BeginLevel, EndLevel
-    file = open("levels.txt", 'r')
+    global BeginLevel, EndLevel, file_name
+    file_name = None
+    user = input("Name of level: ") + ".txt"
+    files = os.listdir(path='levels')
+    findList = []
+    # finds all txt files and puts it in findList
+    for i in range(len(os.listdir(path='levels'))):
+        if files[i].find(".txt") != -1:
+            findList.append(files[i])
+    for i in range(len(findList)):
+        if user == findList[i]:
+            file_name = user
+            print(" == file found >> " + str(user) + " << ")
+    if file_name is None:
+        user = input(" == found no files with that name == \nCreate One? (y or n) : ")
+        if user.upper() != "Y":
+            print(" === returning to game... === ")
+            return
+        if user.upper() == "Y":
+            user = input("File name: ") + ".txt"
+            file_name = user
+            file_name = "levels/" + file_name
+            file = open(file_name, 'x')
+            file.close()
+            print(" == File created == ")
+            return
+
+    file_name = "levels/" + file_name
+    file = open(file_name, 'r')
     file_read = file.read()
     file.close()
     CoList = file_read.split("\n")
+    counter_file = 0
     for i in CoList:
         if i:
             counter_file += 1
     # Getting user input for the file they want to open
     user = input("You have " + str(counter_file) + " levels! Which one do you want to load (give it an integer) : ")
-    counter_file = 0
 
+    # flag
     try:
-        new_user = int(user)
-    except:
-        print("has to be an int")
-        return None
+        int(user)
+    except ValueError:
+        print(" === don't tell me something, say a whole number... === ")
+        return
 
     # == actually loading the level ==
-    file = open("levels.txt", 'r')
+    file = open(file_name, 'r')
     file_lines = file.readlines()
     file.close()
 
     add_object__.reset()
-    level_list_raw = file_lines[new_user - 1]
+    level_list_raw = file_lines[int(user) - 1]
     level_list = dict(eval(level_list_raw))
 
     dict_obstacles = level_list.get("obstacles")
@@ -94,7 +139,6 @@ def load_level():
     # makes the "end level" block
     end_level(override=(EndLevel[1][0], EndLevel[1][1]))
 
-    # YAY!!!
     print(" === Loaded Successfully === ")
 def begin_level(override=(int, int)):
     # flag
@@ -116,4 +160,69 @@ def end_level(override=(int, int)):
         add_object__.make((255, 0, 255), override, (50, 65), cntrl="end level")
         return None
     add_object__.make((255, 0, 255), (x, y), (50, 65), cntrl="end level")
+def merge_levels():
+    files = os.listdir(path='levels')
+    findList = []
+    for i in range(len(os.listdir(path='levels'))):
+        if files[i].find(".txt") != -1:
+            findList.append(files[i])
+    user = input(f"\nWhich levels do you want to merge? \n{findList}\n(say: \"[level], [level]\" ex: \"level1, level2\" level1 would be given all the files from level2)\n:")
 
+    # interpret user
+    find_comma = user.find(",")
+    firstFile = ""
+    for i in range(find_comma):
+        firstFile = str(firstFile) + user[i]
+    find_secondFile = len(firstFile) + 2
+    length_secondFile = abs(find_secondFile - len(user))
+    secondFile = ""
+    for i in range(length_secondFile):
+        secondFile = secondFile + str(user[i + find_secondFile])
+    firstFile, secondFile = firstFile + ".txt", secondFile + ".txt"
+    # flag
+    if firstFile == secondFile:
+        print(" == both files are the same bro.. why? == ")
+        return
+    flag1, flag2 = False, False
+    for i in range(len(findList)):
+        if findList[i] == firstFile:
+            flag1 = True
+        if findList[i] == secondFile:
+            flag2 = True
+    if not flag1 or not flag2:
+        print(" == No file found or you typed something wrong.. == ")
+        return
+    # merge second file with first file
+    with open(f"levels/{secondFile}") as file:
+        while line := file.readline().rstrip():
+            first = open(f"levels/{firstFile}", 'a')
+            first.write((str(line)) + "\n")
+            first.close()
+
+    print(" === Merge Successful === ")
+def create_levels():
+    user = input("level name (say quit to not make one): ")
+    if user.upper() == "QUIT":
+        print("== No level was made == ")
+        return
+    user = user + ".txt"
+    file = open(f"levels/{user}", "x")
+    file.close()
+    print(f" === level {user} was made === ")
+def delete_levels():
+    user = input("input file to be deleted (say quit to return to game): ")
+    if user.upper() == "QUIT":
+        print(" == okay returning to game... == ")
+        return
+    user = user + ".txt"
+    files = os.listdir(path='levels')
+    findList = []
+    for i in range(len(os.listdir(path='levels'))):
+        if files[i].find(".txt") != -1:
+            findList.append(files[i])
+    for i in range(len(findList)):
+        if user == findList[i]:
+            os.remove("levels/" + user)
+            print(f" === okay {user} was deleted === ")
+            return
+    print("File that you inputted was not found ")
